0e8c0437ffc5d8adaadcc3c43a45517dfa90f02d
Alban Bernard <bounechber@gmail.com>
2022-11-10 20:39:24 +0100

    Génération de la grille, début

--- Grille.cs
+++ Grille.cs
@@ -13,32 +13,45 @@ namespace sudoku
     {
         internal class Case : IComparable<Case>
         {
-            static public int xLast = 1;
-            static public int yLast = 1;
-            public int x;
-            public int y;
-            public bool[] possibilites;
-            public int nbPoss;
+            private int x;
+            private int y;
+            private bool[] possibilites;
+            private int nbPoss;
 
-            public Case()
+            public Case(int _x, int _y)
             {
-                x = xLast;
-                y = yLast;
-                if (xLast == 9)
-                {
-                    xLast = 1;
-                    yLast += 1;
-                }
-
+                x = _x;
+                y = _y;
                 possibilites = new bool[9] { true, true, true, true, true, true, true, true, true };
+                nbPoss = possibilites.Length;
             }
 
-            static public void resetLasts()
+            // Retourne une valeur aléatoire parmi celles possibles pour la case
+            public int getRandomPoss()
             {
-                xLast = 0;
-                yLast = 0;
+                // Générer un nombre aléatoire entre 0 et le nombre de possibilités
+                int randNb = Random.Shared.Next(nbPoss);
+                // Récupérer la valeur associée à la randNb-ième valeur possible
+                for(int i = 0; i < possibilites.Length; i++)
+                {
+                    if (possibilites[i] == true)
+                    {
+                        if (randNb == 0)
+                            return i;
+                        randNb--;
+                    }
+                }
+                // Pas de valeur possible ?!
+                return -1;
             }
 
+            // Supprimer la possibilité si elle a deja été utilisée sur l'une des cases déterminantes
+            public void check()
+            {
+
+            }
+
+            // Comparer deux cases, comparaison faites sur leur nombre de possibilités
             public int CompareTo(Case? obj)
             {
                 if (obj == null)
@@ -60,10 +73,26 @@ namespace sudoku
         // Construire Grille en utilisant *Case*[9][9]
         public void GenerateGrid()
         {
+            // ## Etape 1 ##
+            // Tableau de 81 cases (temporaire)
             Case[] cases = new Case[81];
-            Case.resetLasts();
+
+            // Remplissage du tableau
+            for(int y = 0; y < 9; y++)
+                for (int x = 0; x < 9; x++)
+                    cases[y * 9 + x] = new Case(x, y);
+
+            // ## Etape 2 ##
+            // Trier le tableau par ordre croissant
             Array.Sort(cases);
-            cases[0] = new Case();
+
+            // ## Etape 3 ##
+            int index = 0;
+
+            // ## Etape 4 ##
+            int poss = cases[index].getRandomPoss();
+            if (poss == -1)
+                MessageBox.Show("La case " + index + " ne renvoie aucune possibilité !", "Erreur Génération", MessageBoxButtons.OK, MessageBoxIcon.Error);
         }
 
         // Retourne la valeur à la position (x, y)
